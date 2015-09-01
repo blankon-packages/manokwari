d9f3d43b0deeb0c5b83a36ac309cc415dd89b85e
winardiaris@localhost <>
2015-09-01 17:57:25 +0700

    add arrow navigation on search manokwari

--- system/js/search.js
+++ system/js/search.js
@@ -7,6 +7,10 @@
 		$('input').val("");
 		$('#listSearch .ui-listview-item').hide();
 	};
+	function unselect(){
+		$('#listSearch').find('.selected').removeClass('selected');
+		$('#listSearch').find('.active').removeClass('active');
+	}
 	// accending sort
 	function asc_sort(a, b){
 		return ($(b).text()) < ($(a).text()) ? 1 : -1;    
@@ -33,37 +37,74 @@
     var form = $("<div>").attr({"class":"filterform"}),
         input = $("<input>").attr({"class":"filterinput","type":"text","id":"searchinput","placeholder":"Type to search..."});
     $(form).append(input).appendTo(header);
+   
+	//arrow navigation
+   
 
      $(input)
-		.click( function(){firstCond();})
-		.keyup( function () {
-			var filter = $(this).val();
-			$(list).find(".ui-listview-item").hide();
-			if (filter && filter != " " && filter != "" && filter.length > 0) {
-				clearTimeout(typingTimeout);
-				typingTimeout = setTimeout(function(){
-					$(list).hide();				
-
-					// the query executed immediatelly for each keypress, this will cause performance issue
-					// give some delay while typing
-
-					// this finds all links in a list that contain the input,
-					// and hide the ones not containing the input while showing the ones that do
-					$(list).find("a:not(:Contains(" + filter + "))").parent().hide();
-					$(list).find("a:Contains(" + filter + ")").parent().show();
-					$(list).fadeIn();				
-				}, 500);
-			} else {
-				$(list).find(".ui-listview-item").hide();
-			}
-			return false;
-		})
-		.keypress(function(e){
-			if(e.which==13){
-				var a = $(list).find(".ui-listview-item:visible").first().children().attr('desktop');
+		.click( function(){firstCond();unselect();})
+		.keyup( function (e) {
+			 var $item = $('#listSearch a span:visible').not(':has(:empty)');
+			var o = {38: 'up',40: 'bottom',37: 'prev',39: 'next'}
+			var dir = o[e.which];
+		    var $active = $('.active'),
+		        i = $item.index($active);
+			
+			
+			if (e.which == 13) {
+		        //$(list).find('.selected').removeClass('selected');
+		        $('span.active').parent().addClass('selected');
+		        var a = $active.parent().attr('desktop')
 				Utils.run_desktop(a);
+				//alert(a);
 				firstCond();
+				unselect();
+		    }
+		    else if(e.which == 38 || e.which == 40 ){
+				var p = dir === 'up' ? (i - 1) : (i + 1);
+				
+				unselect();
+				
+	            $item.eq(p).addClass('active');
+	            $(list).find(".active").parent().parent().addClass('selected');
 			}
+			// nonaktifkan kanan,kiri,shift,control,alt,tab,menu
+			else if(e.which ==37 || e.which ==39 || e.which ==16 || e.which ==17 || e.which ==18 || e.which ==9 || e.which ==93){
+				return;
+			}
+			else{
+					var filter = $(this).val();
+					$(list).find(".ui-listview-item").hide();
+					if (filter && filter != " " && filter != "" && filter.length > 0) {
+						clearTimeout(typingTimeout);
+						typingTimeout = setTimeout(function(){
+							$(list).hide();				
+		
+							// the query executed immediatelly for each keypress, this will cause performance issue
+							// give some delay while typing
+		
+							// this finds all links in a list that contain the input,
+							// and hide the ones not containing the input while showing the ones that do
+							$(list).find("a:not(:Contains(" + filter + "))").parent().hide();
+							$(list).find("a:Contains(" + filter + ")").parent().show();
+							$(list).fadeIn();
+							
+							unselect();
+											
+							$(list).find('a span:visible').first().addClass('active');
+							$(list).find(".active").parent().parent().addClass('selected');
+							
+						}, 500);
+					} else {
+						$(list).find(".ui-listview-item").hide();
+					}
+					return false;
+			}
+		    
+		    if (!$active.length) {
+		        $item.first().addClass('active');
+		        return;
+		    }
 		});
   }
   //ondomready
@@ -76,5 +117,6 @@
 		listSearch_child.sort(asc_sort).appendTo(listSearch);
 		listSearch_child.mouseup(function() {firstCond();});
 		removeDup(listSearch_child);
+		$("#listApplications").click(function(){ firstCond();});
 	});
 }) (jQuery);																																								
